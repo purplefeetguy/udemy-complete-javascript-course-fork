bf7cc9a549e8584d6e20df3030c4bea61c53cf99
u770572 <jose.castillo@footlocker.com>
2019-07-24 07:29:11 -0500

    basic variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,21 @@
-console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+//var 3years =3;
+//var john/mark = 'John and Mark';
+//var function = 23;
+//var delete = 23;
+
+
